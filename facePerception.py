d57cf91d64fffd0940574641a3b7835ae2b9dd27
Ghoul_Lee <windghoul@gmail.com>
2017-11-29 16:12:25 +0800

    利用opencv识别人脸完成

--- facePerception.py
+++ facePerception.py
@@ -6,9 +6,22 @@ if not cap.isOpened():
     exit(1)
 #读取摄像头并且显示数据
 
-ret,img = cap.read()
-cv2.namedWindow("vedio")
-cv2.imshow("vedio",img)
+while True :
+    ret,img = cap.read()
+    # 人脸识别模块载入
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+    print(faces)
+    for x,y,w,h in faces :
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    cv2.namedWindow("vedio")
+    cv2.imshow("vedio",img)
+    if cv2.waitKey(1) & 0xFF == 27: break
 
 
 # 释放所有资源
+
+cap.release()
+cv2.destroyAllWindows()
